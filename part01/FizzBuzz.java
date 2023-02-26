fca0b8bca2eaecd8c15d4fe5c8030726dfeebc38
Albert Ramos <albertramos.dev@gmail.com>
2023-02-26 14:10:01 -0700

    Add java files

---
+++ part01/FizzBuzz.java
@@ -0,0 +1,26 @@
+
+/*
+ * 'Write a program that prompts the user for a number between one and one hundred, and prints that number. If the number is divisible by three, then print "Fizz" instead of the number. If the number is divisible by five, then print "Buzz" instead of the number. If the number is divisible by both three and five, then print "FizzBuzz" instead of the number.'
+ */
+import java.util.Scanner;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Give a number:");
+        int number = Integer.valueOf(scanner.nextLine());
+
+        if (number % 3 == 0 && number % 5 == 0) {
+            System.out.println("FizzBuzz");
+        } else if (number % 3 == 0) {
+            System.out.println("Fizz");
+        } else if (number % 5 == 0) {
+            System.out.println("Buzz");
+        } else {
+            System.out.println(number);
+        }
+
+        scanner.close();
+    }
+}
