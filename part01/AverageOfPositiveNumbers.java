f3a1863c1a1487417fa9d5c392bf28e1ece60b3c
Albert Ramos <72161221+albertintech@users.noreply.github.com>
2023-02-27 17:16:58 -0700

    Create AverageOfPositiveNumbers.java

---
+++ part01/AverageOfPositiveNumbers.java
@@ -0,0 +1,39 @@
+/*
+Write a program that asks the user for input until the user inputs 0.
+After this, the program prints the average of the numbers. The number
+zero does not need to be counted to the average. You may assume that
+the user inputs at least one number.
+*/
+import java.util.Scanner;
+
+public class AverageOfPositiveNumbers {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int count = 0;
+        double avg = 0;
+        int sum = 0;
+        
+        while (true) {
+            // System.out.println("Give a number:");
+            int userNum = Integer.valueOf(scanner.nextLine());
+            
+            if (userNum == 0) {
+                break;
+            }
+            
+            count = count + 1;
+            sum = userNum + sum;
+            avg = (sum / (count * 1.0));
+        }
+        
+        if (avg == 0) {
+            System.out.println("Cannot calculate the average");
+        } else if (avg < 0) {
+            System.out.println(avg * -1);
+        } else {
+            System.out.println(avg);
+        }
+        scanner.close();
+    }
+}
