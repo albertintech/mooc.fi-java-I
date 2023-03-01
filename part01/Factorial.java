b565f6b872a3f57281438fac324d97c20c3ebd41
Albert Ramos <72161221+albertintech@users.noreply.github.com>
2023-02-28 20:02:48 -0700

    Create Factorial.java

---
+++ part01/Factorial.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class Factorial {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int count = 1;
+        int factorial = 1;
+        
+        System.out.println("Give a number: ");
+        int userNum = Integer.valueOf(scanner.nextLine());
+        int total = 0;
+        
+        while (count <= userNum) {
+            
+            factorial *= count;
+            count++;
+        }
+        System.out.println("Factorial: " + factorial);
+    }
+}
