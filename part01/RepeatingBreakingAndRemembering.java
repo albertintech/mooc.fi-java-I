54a15091c47aecd861eb9311e8128a04ef8d0e31
Albert Ramos <72161221+albertintech@users.noreply.github.com>
2023-03-02 10:39:05 -0700

    Pass Test 5

--- part01/RepeatingBreakingAndRemembering.java
+++ part01/RepeatingBreakingAndRemembering.java
@@ -17,14 +17,23 @@ public class RepeatingBreakingAndRemembering {
         int sum = 0;
         int count = -1;
         double avg = 0;
+        int even = -1;
+        int odd = 0;
         
         System.out.println("Give numbers:");
         
         while (userNum != -1) {
             sum = userNum + sum;
-            System.out.println(sum);
+            // System.out.println(sum);
             count++;
-            System.out.println(count);
+            // System.out.println(count);
+            if (userNum % 2 == 0) {
+                even++;
+            }
+            if (userNum % 2 != 0) {
+                odd++;
+            }
+            
             userNum = Integer.valueOf(scanner.nextLine());
         }
         
@@ -33,6 +42,8 @@ public class RepeatingBreakingAndRemembering {
         System.out.println("Sum: " + sum);
         System.out.println("Numbers: " + count);
         System.out.println("Average: " + avg);
+        System.out.println("Even: " + even);
+        System.out.println("Odd: " + odd);
         scanner.close();
     }
 }
