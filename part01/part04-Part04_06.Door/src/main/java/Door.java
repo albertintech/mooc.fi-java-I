e0811e37d52a3ccb8632b105611120ec3dab855b
Albert Ramos <72161221+albertintech@users.noreply.github.com>
2023-03-13 13:59:29 -0700

    Create Door.java

---
+++ part01/part04-Part04_06.Door/src/main/java/Door.java
@@ -0,0 +1,14 @@
+/**
+ *
+ * @author 101748
+ */
+
+public class Door {
+    public void door() {
+        // no variables or state
+    }
+    
+    public void knock() {
+        System.out.println("Who's there?");
+    }
+}
