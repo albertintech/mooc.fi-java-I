82802bc1616edbc1ec051f0e90a04e4d52099c24
Albert Ramos <72161221+albertintech@users.noreply.github.com>
2023-03-13 19:52:09 -0700

    Update MainProgram.java

--- part01/part04-Part04_08.DecreasingCounter/src/main/java/MainProgram.java
+++ part01/part04-Part04_08.DecreasingCounter/src/main/java/MainProgram.java
@@ -2,10 +2,15 @@
 public class MainProgram {
 
     public static void main(String[] args) {
-        DecreasingCounter counter = new DecreasingCounter(10);
+        DecreasingCounter counter = new DecreasingCounter(100);
 
         counter.printValue();
         counter.decrement();
+        counter.printValue();
         counter.decrement();
+        counter.reset();
+        counter.printValue();
+        counter.decrement();
+        counter.printValue();
     }
 }
