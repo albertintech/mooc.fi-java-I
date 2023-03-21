1446bba611f94564d0925e9c76bde6c7ce25751d
Albert Ramos <72161221+albertintech@users.noreply.github.com>
2023-03-20 19:15:47 -0700

    Update Cubes.java

--- part01/part04-Part04_22.Cubes/src/main/java/Cubes.java
+++ part01/part04-Part04_22.Cubes/src/main/java/Cubes.java
@@ -4,7 +4,18 @@ import java.util.Scanner;
 public class Cubes {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
+        Scanner sc = new Scanner(System.in);
+        
+        while (true) {
+            String row = sc.nextLine();
+            
+            if (row.equals("end")) {
+                break;
+            }
+            
+            int number = Integer.valueOf(row);
+            number = number * number * number;
+            System.out.println(number);
+        }
     }
 }
