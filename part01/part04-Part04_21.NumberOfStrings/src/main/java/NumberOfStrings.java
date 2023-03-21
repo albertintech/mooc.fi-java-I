16b7695fcd707327ffc248ad7d60770e1217f6ea
Albert Ramos <72161221+albertintech@users.noreply.github.com>
2023-03-20 18:10:35 -0700

    Update NumberOfStrings.java

--- part01/part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
+++ part01/part04-Part04_21.NumberOfStrings/src/main/java/NumberOfStrings.java
@@ -5,6 +5,17 @@ public class NumberOfStrings {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int count = -1;
+        
+        while (true) {
+            String line = scanner.nextLine();
+            count++;
+            
+            if(line.equals("end")) {
+                break;
+            }
+        }
+        
+        System.out.println(count);
     }
 }
