4e2107c8d43a96e41ef232506b59de9bff503b88
Albert Ramos <72161221+albertintech@users.noreply.github.com>
2023-03-23 07:42:47 -0700

    Update IsItInTheFile.java

--- part01/part04-Part04_27.IsItInTheFile/src/main/java/IsItInTheFile.java
+++ part01/part04-Part04_27.IsItInTheFile/src/main/java/IsItInTheFile.java
@@ -12,6 +12,23 @@ public class IsItInTheFile {
 
         System.out.println("Search for:");
         String searchedFor = scanner.nextLine();
-
+        
+        boolean found = false;
+        
+        try (Scanner sc = new Scanner(Paths.get(file))) {
+            while (sc.hasNextLine()) {
+                String row = sc.nextLine();
+                if (row.equals(searchedFor)) {
+                    found = true;
+                }
+            }
+            if (found) {
+                System.out.println("Found!");
+            } else {
+                System.out.println("Not found.");
+            }
+        } catch (Exception e) {
+            System.out.println("Reading the file " + file + " failed.");
+        }
     }
 }
